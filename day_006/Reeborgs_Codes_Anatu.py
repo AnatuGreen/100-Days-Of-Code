0a29b66b289499db7d47463b518ee8ccb1ac98dc
Anaturuchi <anatuonline@gmail.com>
2025-07-27 22:12:45 +0100

    Update Reeborgs_Codes_Anatu.py

--- day_006/Reeborgs_Codes_Anatu.py
+++ day_006/Reeborgs_Codes_Anatu.py
@@ -40,5 +40,33 @@ def one_jump():
 for jump in range(1,7):
     one_jump()
 
+#Level 3
 
+#Make turn right function using 3 turn lefts
+def turn_right():
+     turn_left()
+     turn_left()
+     turn_left()
+
+#All Required for each complete jump shall be made into a function
+def one_jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+#We need 6 jumps to complete challenge so define loop with range
+ 
+#for jump in range(1,7):
+    #print(at_goal())
+while at_goal() == False: #can also use 'while not at_goal()' or 'at_goal() == !True
+    one_jump()
+done()
+
+
+#level 3 ends
 
